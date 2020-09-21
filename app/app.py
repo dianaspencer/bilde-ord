cd4b330102a94f2c1db87cf40cfbf620eb8e8d75
Diana Spencer <diana.ponce.spencer@gmail.com>
2020-09-21 12:39:56 +0200

    init starter for rendering image sent

--- app/app.py
+++ app/app.py
@@ -1,29 +1,37 @@
 import logging
+import flask
 
 from flask import Flask
-from flask import render_template
-from flask import request
 
 app = Flask(__name__)
 # Confirm file extension is a valid image
-app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
+app.config['IMAGE_EXTENSIONS'] = ['.jpg', '.png', '.gif']
 
 logging.basicConfig(level=logging.DEBUG)
+stdout = app.logger
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return flask.render_template('index.html')
 
 
 # TODO review and validate the file submission
 # TODO raise exceptions if missing, too much, or wrong data
+# TODO find an easier way to perform stdout
 @app.route('/', methods=['POST'])
-def upload_image():
-    app.logger.info(request.files)  # TODO find an easier way to perform stdout
-    raw_image = request.files['image_file']
-    print(raw_image)
-    return "Done"
+def upload():
+    unverified_data = flask.request.files['image']
+    # file_name, file_ext = unverified_data.filename.split('.')
+    # if len(file_name) == '' or file_ext not in app.config['IMAGE_EXTENSIONS']:
+    #    abort(400)
+    img = unverified_data.read()
+    return flask.redirect(flask.url_for('index'))
+
+
+@app.route('/')
+def render():
+    pass
 
 
 if __name__ == "__main__":
